c19aa351a277d8dbdf4e6a6f8a753e324578cbda
AngelosDev <angelde9919@gmail.com>
2026-08-26 15:26:25 -0600

    62 esperar argumentos especificos

--- 02-primeros-pasos/src/FirstSteps.test.tsx
+++ 02-primeros-pasos/src/FirstSteps.test.tsx
@@ -1,27 +1,62 @@
-import { describe, expect, test, vi } from "vitest";
+import {  afterEach, describe, expect, test, vi } from "vitest";
 import { FirstSteps } from "./FirstSteps";
 import { render, screen } from "@testing-library/react";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const mockItemCounter = vi.fn((_props: unknown) => {
+  return <div data-testid="ItemCounter" />;
+});
 
-vi.mock('./shopping-cart/ItemCounter', () =>({
-  ItemCounter: () => <div data-testid="ItemCounter"></div>
-}))
+vi.mock("./shopping-cart/ItemCounter", () => ({
+  ItemCounter: (props: unknown) => mockItemCounter(props),
+}));
+
+// vi.mock("./shopping-cart/ItemCounter", () => ({
+//   ItemCounter: (props: unknown) => (
+//     <div
+//       data-testid="ItemCounter"
+//       name={props.name}
+//       quantity={props.quantity}
+//     />
+//   ),
+// }));
 
 describe("First steps", () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   test("shoud match snapshot", () => {
     const { container } = render(<FirstSteps />);
 
     expect(container).toMatchSnapshot();
   });
 
-  test('should render the correct number of ItemCounter componets', () =>{
-    render(<FirstSteps/>)
-    
-    const itemCounters = screen.getAllByTestId('ItemCounter')
+  test("should render the correct number of ItemCounter componets", () => {
+    render(<FirstSteps />);
 
-    expect(itemCounters.length).toBe(3)
+    const itemCounters = screen.getAllByTestId("ItemCounter");
 
-    screen.debug()
-    
-  })
+    expect(itemCounters.length).toBe(3);
+
+    screen.debug();
+  });
+
+  test("should render item counter with correc props", () => {
+    render(<FirstSteps />);
+
+    expect(mockItemCounter).toHaveBeenCalledTimes(3);
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: "Nintendo swtich2xdd",
+      quantity: 10,
+    });
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: "exbos",
+      quantity: 0,
+    });
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: "diyin light 3 :000",
+      quantity: 2,
+    });
+  });
 });
