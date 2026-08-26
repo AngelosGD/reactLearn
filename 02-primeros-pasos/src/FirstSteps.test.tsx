c8695a494fe8dfda8dfb3a41da4f3e8d0c338e71
AngelosDev <angelde9919@gmail.com>
2026-08-26 14:49:41 -0600

    61 - componentes fiction - mockups con vi

--- 02-primeros-pasos/src/FirstSteps.test.tsx
+++ 02-primeros-pasos/src/FirstSteps.test.tsx
@@ -1,6 +1,11 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { FirstSteps } from "./FirstSteps";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+
+
+vi.mock('./shopping-cart/ItemCounter', () =>({
+  ItemCounter: () => <div data-testid="ItemCounter"></div>
+}))
 
 describe("First steps", () => {
   test("shoud match snapshot", () => {
@@ -8,4 +13,15 @@ describe("First steps", () => {
 
     expect(container).toMatchSnapshot();
   });
+
+  test('should render the correct number of ItemCounter componets', () =>{
+    render(<FirstSteps/>)
+    
+    const itemCounters = screen.getAllByTestId('ItemCounter')
+
+    expect(itemCounters.length).toBe(3)
+
+    screen.debug()
+    
+  })
 });
