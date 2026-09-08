337d419c25e5764d93e439fc6895e1e44855b864
AngelosDev <angelde9919@gmail.com>
2026-09-08 13:57:31 -0600

    87 correcto se me paso 1 xd entonces este es ek 87 y los otros 86-85 y asim
    -las devTools son para ver un arbol de componentes por asi decirlo
    -los hooks son posicionales, es decir react los identifica dependiendo de cual esta antes o despues
    
    -se recomienda los hooks de useState antes que los de useEffect, esos despues

--- 03-gifts-app/src/main.tsx
+++ 03-gifts-app/src/main.tsx
@@ -2,11 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GiftsApp } from './GiftsApp'
 import './index.css'
-import { MyCounterApp } from './counter/components/MyCounterApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <GiftsApp/> */}
-    <MyCounterApp/>
+    <GiftsApp/>
+    {/* <MyCounterApp/> */}
   </StrictMode>,
 )
