16ffe6383bcdeccbaf39ab161573b344532f4ae6
AngelosDev <angelde9919@gmail.com>
2026-09-09 14:28:53 -0600

    91- subir a netlify o probar con http-server
    npm run build comando que hace la build del proyecto, tomar en cuenta los errores que menciona si no llega a hacer la build

--- 03-gifts-app/src/gifs/interfaces/giphy.response.ts
+++ 03-gifts-app/src/gifs/interfaces/giphy.response.ts
@@ -12,7 +12,6 @@ export interface GiphyGif {
   bitly_gif_url: string;
   bitly_url: string;
   embed_url: string;
-  username: Username;
   source: string;
   title: string;
   rating: Rating;
@@ -21,7 +20,6 @@ export interface GiphyGif {
   source_post_url: string;
   is_sticker: number;
   import_datetime: Date;
-  trending_datetime: Date | TrendingDatetimeEnum;
   images: Images;
   analytics_response_payload: string;
   analytics: Analytics;
@@ -57,7 +55,6 @@ export interface Images {
   fixed_width_small: FixedHeight;
   fixed_width_small_still: The480_WStill;
   fixed_width_still: The480_WStill;
-  looping: Looping;
   original_still: The480_WStill;
   original_mp4: DownsizedSmall;
   preview: DownsizedSmall;
@@ -94,27 +91,18 @@ export interface FixedHeight {
   hash?: string;
 }
 
-export interface Looping {}
 
-export enum Rating {
-  G = "g",
-  PG = "pg",
-}
+export type Rating = 'G' | 'PG'
 
-export enum TrendingDatetimeEnum {
-  The00000000000000 = "0000-00-00 00:00:00",
-}
 
-export enum Type {
-  GIF = "gif",
-}
+
+export type Type = 'gif'
 
 export interface User {
   avatar_url: string;
   banner_image: string;
   banner_url: string;
   profile_url: string;
-  username: Username;
   display_name: string;
   description: string;
   instagram_url: string;
@@ -122,12 +110,6 @@ export interface User {
   is_verified: boolean;
 }
 
-export enum Username {
-  AizenSosuke9641 = "aizen_sosuke9641",
-  Empty = "",
-  Kingofkingkillers = "kingofkingkillers",
-  Xemirgms = "xemirgms",
-}
 
 export interface Meta {
   status: number;
