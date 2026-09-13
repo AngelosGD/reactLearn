e6fd2777a66b93c013432ac507420cde096863d6
AngelosDev <angelde9919@gmail.com>
2026-09-12 20:48:28 -0600

    107 - mas pruebas sobre el usegifs xd, pruebas para ver que no pase de los 8 terminos previos etc, y manejar cache

--- 03-gifts-app/src/gifs/hooks/useGifs.test.tsx
+++ 03-gifts-app/src/gifs/hooks/useGifs.test.tsx
@@ -1,7 +1,8 @@
 import { renderHook } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { useGifs } from "./useGifs";
 import { act } from "react";
+import * as gifActions from "../actions/get-gifs-by-query.action";
 
 describe("useGifs", () => {
   test("should return default values and methods", () => {
@@ -16,20 +17,52 @@ describe("useGifs", () => {
   test("should return a list of gifs", async () => {
     const { result } = renderHook(() => useGifs());
 
-    await act(async() => {
+    await act(async () => {
       await result.current.handleSearch("goku");
     });
 
     expect(result.current.gifs.length).toBe(10);
   });
 
-  test('should return a list of gifs when handleTermClicked  is called', async () =>{
-    const {result} = renderHook(() => useGifs())
+  test("should return a list of gifs when handleTermClicked  is called", async () => {
+    const { result } = renderHook(() => useGifs());
 
-    await act(async() =>{
-      await result.current.handleTermClicked('goku')
+    await act(async () => {
+      await result.current.handleTermClicked("goku");
+    });
+
+    expect(result.current.gifs.length).toBe(10);
+  });
+
+  test("should return a list of gifs from cache", async () => {
+    const { result } = renderHook(() => useGifs());
+
+    await act(async () => {
+      await result.current.handleTermClicked("goku");
+    });
+
+    expect(result.current.gifs.length).toBe(10);
+
+    vi.spyOn(gifActions, "getGifsByQuery").mockRejectedValue(
+      new Error("esto no va endejo :,vvv xddd"),
+    );
+
+    await act(async () => {
+      await result.current.handleTermClicked("goku");
+    });
+
+    expect(result.current.gifs.length).toBe(10);
+  });
+
+  test("should return no more than 8 previous terms", async() => {
+    const { result } = renderHook(() => useGifs());
+    
+    vi.spyOn(gifActions, "getGifsByQuery").mockResolvedValue([]);
+
+    await act(async () =>{
+      await result.current.handleSearch('goku1')
     })
 
-    expect(result.current.gifs.length).toBe(10)
-  })
+    console.log(result.current.previousTerms)
+  });
 });
