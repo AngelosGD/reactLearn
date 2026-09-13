bc46449a615542f079d4a9d890f366648bc663f2
AngelosDev <angelde9919@gmail.com>
2026-09-12 21:33:33 -0600

    109 a 112 acabados -seccion de testings del gifs app lista -sigue profundizar con hooks -retomamos el % de cobertura (coverage en el package.json)

--- 03-gifts-app/src/shared/components/SearchBar.test.tsx
+++ 03-gifts-app/src/shared/components/SearchBar.test.tsx
@@ -13,18 +13,45 @@ describe("search bar", () => {
   });
 
   test("should could on query with the correct value (700ms)", async () => {
-    const onQuery = vi.fn()
+    const onQuery = vi.fn();
     render(<SearchBar onHandleSearch={onQuery} />);
-    
-    const input = screen.getByRole('textbox')
 
-    fireEvent.change(input, {target: {value: 'test'}})
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "test" } });
 
-    await waitFor(() =>{
-      expect(onQuery).toHaveBeenCalledWith('test')
-      expect(onQuery).toHaveBeenCalled()
-      
-    })
-    
+    await waitFor(() => {
+      expect(onQuery).toHaveBeenCalledWith("test");
+      expect(onQuery).toHaveBeenCalled();
+    });
+  });
+
+  test("should call only once with the last value (debounce)", async () => {
+    const onQuery = vi.fn();
+    render(<SearchBar onHandleSearch={onQuery} />);
+
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "t" } });
+    fireEvent.change(input, { target: { value: "te" } });
+    fireEvent.change(input, { target: { value: "tes" } });
+    fireEvent.change(input, { target: { value: "test" } });
+
+    await waitFor(() => {
+      expect(onQuery).toHaveBeenCalledTimes(1);
+      expect(onQuery).toHaveBeenCalledWith("test");
+    });
+  });
+
+  test("should call onQuery when button clicked with the input value", () => {
+    const onQuery = vi.fn();
+    render(<SearchBar onHandleSearch={onQuery} />);
+
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "test" } });
+
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+
+    expect(onQuery).toHaveBeenCalledTimes(1)
+    expect(onQuery).toHaveBeenCalledWith('test')
   });
 });
