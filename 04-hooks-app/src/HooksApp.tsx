436d6fc7d08b4c226d5f0ea3e46ae12825944214
AngelosDev <angelde9919@gmail.com>
2026-09-14 16:38:44 -0600

    118- seccion nueva hareos una app cn varios hooks, simul,ando un semaforo, se creo el archivo TrafficsLight.tsx donde se usa un state para cambiar entre colores dependiendo del boton, dandole una clase o objeto de colors donde declaramos los colores y su valor o correspondinete en tailwind

---
+++ 04-hooks-app/src/HooksApp.tsx
@@ -0,0 +1,10 @@
+
+export const HooksApp = () => {
+  return (
+    <div className="bg-gradient">
+        <h1 className="text-3xl font-bold underline">
+            Hola hooks app :D
+        </h1>
+    </div>
+  )
+}
