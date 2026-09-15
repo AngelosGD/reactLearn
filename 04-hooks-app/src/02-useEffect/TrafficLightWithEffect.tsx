406832d30cd4dc897a2daa1c6ee3f8c7e0ff01b2
AngelosDev <angelde9919@gmail.com>
2026-09-14 22:32:50 -0600

    121 recomendaciones del useEffect
    -se recomienda tener mas de 1 effect, ya que deben de ser atomicos, es decir no poner mucha logica en un solo effect

--- 04-hooks-app/src/02-useEffect/TrafficLightWithEffect.tsx
+++ 04-hooks-app/src/02-useEffect/TrafficLightWithEffect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 const colors = {
   red: "bg-red-500 animate-pulse",
@@ -11,51 +11,84 @@ const colors = {
 // type TrafficLightColor = 'red' | 'yellow' | 'green'
 
 // ! mas avanzado :D
-type TrafficLightColor = keyof typeof colors
-
+type TrafficLightColor = keyof typeof colors;
 
 export const TrafficLightWithEffect = () => {
   const [light, setLight] = useState<TrafficLightColor>("red");
-  const [countdown, setCountdown] = useState(5)
+  const [countdown, setCountdown] = useState(5);
 
-  useEffect(() =>{
 
-    if (countdown === 0) return
+  // ? effect para el countdown
+  useEffect(() => {
+    if (countdown === 0) return;
     const intervalId = setInterval(() => {
-      setCountdown(prev => prev-1)
+      setCountdown((prev) => prev - 1);
     }, 1000);
 
-    return () =>{
-      console.log('cleanup effect')
-      clearInterval(intervalId)
-    }
+    return () => {
+      console.log("cleanup effect");
+      clearInterval(intervalId);
+    };
+  }, [countdown]);
 
-  },[countdown])
+  // ! se recomienda tener efectos atomicos, es decir en este caos 2 diferentes, uno para el countodwn y otro para cambiar de luces
+  // ! no tener ningun efecto de varias funciones o fines D:
 
-//   const handleYellow = () =>{
-//     setLight('yellow')
-//   }
-//   const handleRed = () =>{
-//     setLight('red')
-//   }
-//   const handleGreen = () =>{
-//     setLight('green')
-//   }
+  //? change light color effect
+  useEffect(() => {
+
+    if(countdown > 0) return
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setCountdown(5);
+      if (light === "red") {
+        setLight("green");
+        return;
+      }
+
+      if (light === "yellow") {
+        setLight("red");
+        return;
+      }
+
+      if (light === "green") {
+        setLight("yellow");
+        return;
+      }
+  }, [countdown, light]);
+
+  //   const handleYellow = () =>{
+  //     setLight('yellow')
+  //   }
+  //   const handleRed = () =>{
+  //     setLight('red')
+  //   }
+  //   const handleGreen = () =>{
+  //     setLight('green')
+  //   }
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
       <div className="flex flex-col items-center space-y-8">
-
-        <h1 className="text-white text-3xl font-thin">Semaforo con useEffect</h1>
+        <h1 className="text-white text-3xl font-thin">
+          Semaforo con useEffect
+        </h1>
         <h2 className="text-white text-xl">Countdown: {countdown}</h2>
+        
+        <div className=" w-64 bg-gray-700 rounded-full h-2">
+          <div className="bg-blue-500 h-2 rounded-full transition-all duration-1000 ease-linear" style={{width: `${(countdown/5)*100}%`}}>
+          </div>
+        </div>
+        
         <div
           className={`w-32 h-32 ${light === "red" ? colors[light] : "bg-gray-500"} rounded-full`}
         ></div>
 
-        <div className={`w-32 h-32 ${light === 'yellow' ? colors[light] : "bg-gray-500"} rounded-full`}></div>
-        <div className={`w-32 h-32 ${light === 'green' ? colors[light] : "bg-gray-500"} rounded-full`}></div>
-
-        
+        <div
+          className={`w-32 h-32 ${light === "yellow" ? colors[light] : "bg-gray-500"} rounded-full`}
+        ></div>
+        <div
+          className={`w-32 h-32 ${light === "green" ? colors[light] : "bg-gray-500"} rounded-full`}
+        ></div>
       </div>
     </div>
   );
