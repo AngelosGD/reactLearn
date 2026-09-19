5c28f6e59669dc2451792199e1215f618b7ba6bf
AngelosDev <angelde9919@gmail.com>
2026-09-19 17:35:15 -0600

    138 persistencia en el localStorage
    el navegador tiene distintos tipos de bases de datos internas, de sesion, local, entre otras mas, en este caso usaremos la localStorage la cual nos ayudara a guardar los todos
    
    simplemente creamos un effect ya que al ser algo secundario tiene que ser un effect donde simplemente mandamos llamar la funcion localStorage.setItem, esta pide 2 valores
    key: es una key o identificador conla cual tomamos eso guardado
    value: el valor xd, en este caso el state, tenemos q pasarlo con un json.stringify primero para que sea entendible ya que solo acepta STRINGS D:
    
    para que se quede guarado en el useReducer donde pasamos el initialState (getTasksInitialState)
    ponemos una variable que tomara el localStorage.getItem con la key que le pusimos al setItem
    
    si esta vacio ahora si retornamos lo antes del objeto vacio D:

--- 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
@@ -1,4 +1,3 @@
-
 interface Todo {
   id: number;
   text: string;
@@ -20,15 +19,20 @@ export type TaskAction =
   | { type: "TOGGLE_TODO"; payload: number }
   | { type: "DELETE_TODO"; payload: number };
 
+export const getTasksInitialState = (): TaskState => {
+  const localStorageState = localStorage.getItem("tasks-state");
 
-export const getTasksInitialState = (): TaskState =>{
-    return{
-        todos: [],
-        completed: 0,
-        pending:0,
-        length:0
-    }
-}
+  if (!localStorageState) {
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+  }
+  //! el objeto puede haber sido modifica CUIDADO
+  return JSON.parse(localStorageState);
+};
 // ! los reducers son funciones que siempre devuelvan un nuevo estado o un valor, no podemos retornar sin este nuevo estado o valor
 // ! siempre debe regresar el tipo de state que se le dio en este caso TaskState
 
@@ -37,73 +41,67 @@ export const tasksReducer = (
   state: TaskState,
   action: TaskAction,
 ): TaskState => {
-    // = lo del TasakState, sera basado en la action y el state previo y nuevo, siempre
-    // siempre regresare el estado actual D:
+  // = lo del TasakState, sera basado en la action y el state previo y nuevo, siempre
+  // siempre regresare el estado actual D:
 
-    // ? es comun ver swtiches en reducers
+  // ? es comun ver swtiches en reducers
 
-    switch(action.type){
-        // ? siempre se necesita regresar un nuevo estado, o el estado actual (state)
-        case 'ADD_TODO':
-            {
-                const newTodo: Todo = {
-                    id: Date.now(),
-                    // ? como no tenemos el valor del inputValue mandamos el payload que le pusimos caon
-                    text: action.payload,
-                    completed:false
-                }
-                // ! NO SE PUEDE HACER, NO SE PUEDE MUTAR UN STATE, SE REGRESA UNO NUEVO D:
-                // ! state.todos.push()
+  switch (action.type) {
+    // ? siempre se necesita regresar un nuevo estado, o el estado actual (state)
+    case "ADD_TODO": {
+      const newTodo: Todo = {
+        id: Date.now(),
+        // ? como no tenemos el valor del inputValue mandamos el payload que le pusimos caon
+        text: action.payload,
+        completed: false,
+      };
+      // ! NO SE PUEDE HACER, NO SE PUEDE MUTAR UN STATE, SE REGRESA UNO NUEVO D:
+      // ! state.todos.push()
 
-                // * ahora si, barremos el estado, y como debemos regresar un 
-                // * estado nuevo, ponemos los todos en el ...state.todos y agregando el newTodo, la logica del mismo
-                return {
-                    ...state,
-                    todos: [...state.todos, newTodo],
-                    length: state.todos.length + 1,
-                    pending: state.pending + 1,
-
-
-
-                }
-            }
-
-        case 'DELETE_TODO':
-            {
-                const currentTodos = state.todos.filter((todo) => todo.id !== action.payload)
-                return {
-                    ...state,
-                    // ? este filter regresa UN NUEVO ARREGLO, lo cual es lo que buscamos
-                    // ? parecido arriba le pasamos el action,payload en el id que es lo que pusimos como el properti
-                    todos: currentTodos,
-                    length: state.todos.length,
-                    completed: currentTodos.filter((todo) => todo.completed).length,
-                    pending: currentTodos.filter((todo) => !todo.completed).length
-                    
-
-                }
-            }
-
-        case 'TOGGLE_TODO': { 
-                const updatedTodos = state.todos.map((todo) =>{
-                if(todo.id === action.payload){
-                    return{...todo, completed: !todo.completed}
-                }
-                return todo
-            })
-            return {
-                ...state,
-                todos: updatedTodos,
-                completed: updatedTodos.filter((todo) => todo.completed).length,
-                pending: updatedTodos.filter((todo) => !todo.completed).length
-                    
-            } 
-        }
-
-        // ? siempre debe regresar el state, ya sea actual, por eso el default y retornamos el state
-        default:
-            return state;
+      // * ahora si, barremos el estado, y como debemos regresar un
+      // * estado nuevo, ponemos los todos en el ...state.todos y agregando el newTodo, la logica del mismo
+      return {
+        ...state,
+        todos: [...state.todos, newTodo],
+        length: state.todos.length + 1,
+        pending: state.pending + 1,
+      };
     }
-    
-    return state;
+
+    case "DELETE_TODO": {
+      const currentTodos = state.todos.filter(
+        (todo) => todo.id !== action.payload,
+      );
+      return {
+        ...state,
+        // ? este filter regresa UN NUEVO ARREGLO, lo cual es lo que buscamos
+        // ? parecido arriba le pasamos el action,payload en el id que es lo que pusimos como el properti
+        todos: currentTodos,
+        length: state.todos.length,
+        completed: currentTodos.filter((todo) => todo.completed).length,
+        pending: currentTodos.filter((todo) => !todo.completed).length,
+      };
+    }
+
+    case "TOGGLE_TODO": {
+      const updatedTodos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+      return {
+        ...state,
+        todos: updatedTodos,
+        completed: updatedTodos.filter((todo) => todo.completed).length,
+        pending: updatedTodos.filter((todo) => !todo.completed).length,
+      };
+    }
+
+    // ? siempre debe regresar el state, ya sea actual, por eso el default y retornamos el state
+    default:
+      return state;
+  }
+
+  return state;
 };
