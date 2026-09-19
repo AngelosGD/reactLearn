4f93f9f2a102a540f52e57b5b3f089922007e022
AngelosDev <angelde9919@gmail.com>
2026-09-19 14:40:04 -0600

    132 - useReducer
    hooks dificil D:
    lo que llevo es un hook que solo recibe y juega con los parametros que se le den, normalmente son 2
    
    haremos 2 apps aqui, una de una todo list y otra de un juego de palabras adivina palabra D:

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { FocusScreen } from './04-useRef/FocusScreen'
+import { TasksApp } from './05-useReducer/TaskApp'
+// import { FocusScreen } from './04-useRef/FocusScreen'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithEffect/> */}
     {/* <TrafficLightWithHook/> */}
     {/* <PokemonPage/> */}
-    <FocusScreen/>
+    {/* <FocusScreen/> */}
+    <TasksApp/>
   </StrictMode>,
 )
