26df0ce6a0bb40b205577ec7681605dfe08d2784
AngelosGD <angelde9919@gmail.com>
2026-09-20 20:15:54 -0600

    143 logica reducer dejamos el componente principal limpio pra despues hacer su respectiva logica en el reducer

--- 04-hooks-app/src/05-useReducer/reducer/scrambleWordsReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/scrambleWordsReducer.ts
@@ -1,19 +1,21 @@
 export interface ScrambeWordsState {
   currentWord: string;
   errorCounter: number;
-  guess: string; 
+  guess: string;
   isGameOver: boolean;
   maxAllowErrors: number;
   maxSkips: number;
   points: number;
   scrambledWord: string;
   skipCounter: number;
-  words: string[]
+  words: string[];
+  // ? para las game words
+  totalWords: number;
 }
 
-export type ScrambledWordsActions =
-| {type: 'NO_TENGO_LA_MENOR_IDEA_DE_CUALES_ACCIONES_NECESITO'
-}
+export type ScrambledWordsActions = {
+  type: "NO_TENGO_LA_MENOR_IDEA_DE_CUALES_ACCIONES_NECESITO";
+};
 
 const GAME_WORDS = [
   "REACT",
@@ -35,7 +37,6 @@ const GAME_WORDS = [
   "TAILWIND",
 ];
 
-
 // Esta función mezcla el arreglo para que siempre sea aleatorio
 const shuffleArray = (array: string[]) => {
   return array.sort(() => Math.random() - 0.5);
@@ -49,31 +50,35 @@ const scrambleWord = (word: string = "") => {
     .join("");
 };
 
-export const getInitialState=():ScrambeWordsState=>{
-    const shuffledWords = shuffleArray([...GAME_WORDS])
-    
-    
-    return{
-        currentWord: shuffledWords[0],
-        errorCounter: 0,
-        guess: '',
-        isGameOver: false,
-        maxAllowErrors: 3,
-        maxSkips: 3,
-        points: 0,
-        scrambledWord: scrambleWord(shuffledWords[0]),
-        skipCounter: 0,
-        words:   shuffledWords,
-    }
-}
+export const getInitialState = (): ScrambeWordsState => {
+  const shuffledWords = shuffleArray([...GAME_WORDS]);
 
+  return {
+    currentWord: shuffledWords[0],
+    errorCounter: 0,
+    guess: "",
+    isGameOver: false,
+    maxAllowErrors: 3,
+    maxSkips: 3,
+    points: 0,
+    scrambledWord: scrambleWord(shuffledWords[0]),
+    skipCounter: 0,
+    words: shuffledWords,
+    totalWords: shuffledWords.length,
+  };
+};
 
-exort type ScrambleWordAction = 
-| {type:'NO'}
+export type ScrambleWordAction =
+  | { type: "NO_TENGO_LA_MENOR_IDEA_DE_QUE_ACCIONES_NECESITO" }
+  | { type: "NO_TENGO_LA_MENOR_IDEA_DE_QUE_ACCIONES_NECESITO2" }
+  | { type: "NO_TENGO_LA_MENOR_IDEA_DE_QUE_ACCIONES_NECESITO3" };
 
-export const scrambleWordsReducer = (state: ScrambeWordsState, action: ScrambledWordsActions) =>{
-    switch(action.type) {
-        default:
-            return state
-    }
-}
+export const scrambleWordsReducer = (
+  state: ScrambeWordsState,
+  action: ScrambledWordsActions,
+) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
