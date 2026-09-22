cf7a87e5a5db7f6fd5224a1681b6f622a1c609bc
AngelosDev <angelde9919@gmail.com>
2026-09-22 16:06:24 -0600

    166 contexto del usuario
    creamos el authStatus el cual lo daremos al UserContextProps el cual le definimos los estados los cuales son del AuthStates y el gist que copiamos del curso (la interface y los campos del User)
    
    y sus respectivos metodos (login-logout)

--- react-ts-hooks-app-fin-seccion-11/react-ts-hooks-app-fin-seccion-11/src/09-useContext/context/UserContext.tsx
+++ react-ts-hooks-app-fin-seccion-11/react-ts-hooks-app-fin-seccion-11/src/09-useContext/context/UserContext.tsx
@@ -1,17 +1,49 @@
-import { type PropsWithChildren } from "react"
+import { createContext, useState, type PropsWithChildren } from "react";
+import type { User } from "../data/user-mock-data";
 
 // interface UserContextProps {
 //     children: React.ReactNode
 // }
 
+type AuthStatus = "checking" | "authenticated" | "not authenticated";
+
+interface UserContextProps {
+  // ? estados / state
+  authStatus: AuthStatus;
+  user?: User | null;
+
+  // * Methods / Metodos
+  login: (userId: number) => boolean;
+  logout: () => void;
+}
+
+// ? si lo declaramos con <> habra error ya que no esta inicializado, entonces creamos un objeto vacio y ponemos as UserContextProps
+export const UserContext = createContext({} as UserContextProps);
 
 // ? ya que PROVEE estadp
-export const UserContextProvider = ({children}: PropsWithChildren) => {
-  
-    return (
-    <div>
-        <h1>Hola</h1>
-        {children}
-    </div>
-  )
-}
+export const UserContextProvider = ({ children }: PropsWithChildren) => {
+  // ? creamos los estados para el authStatus y user simplementes con el usestate y asignando sus tipos
+  const [authStatus, setAuthStatus] = useState<AuthStatus>("checking");
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogin = (userId: number) => {
+    console.log({ userId });
+    return true;
+  };
+
+  const handleLogout = () => {
+    console.log("logout");
+  };
+  return (
+    <UserContext
+      value={{
+        authStatus: authStatus,
+        user: user,
+        login: handleLogin,
+        logout: handleLogout,
+      }}
+    >
+      {children}
+    </UserContext>
+  );
+};
